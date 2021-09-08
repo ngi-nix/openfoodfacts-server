45c3870b9d495bb56d8f5e966406f4d172064dba
thomassdk <thomassdk@pm.me>
2021-09-08 10:49:39 +0100

    Set volumes and networks with arion

--- arion-compose.nix
+++ arion-compose.nix
@@ -1,80 +1,93 @@
 { pkgs, ... }:
 let
-  inherit (pkgs) lib;
-  networks = [ "webnet" ];
-in {
-  config.project.name = "openfoodfacts-server";
-  config.docker-compose.raw = {
-    "volumes" = [ "dbdata:" ];
+  lib = pkgs.lib;
+  inherit (lib) toString mkForce;
+  networkName = "webnet";
+  networks = [ networkName ];
+  volumes = {
+    dbdata = null;
+    pgdata = null;
+    podata = null;
   };
-  config.services = {
+in {
+  config = {
+    project.name = "openfoodfacts-server";
 
-    mongodb.service = {
-      image = "mongo:4.4";
-      volumes = [ "dbdata:/var/lib/mongodb" ];
-      inherit networks;
+    docker-compose.raw = {
+      networks = { ${networkName} = null; };
+      inherit volumes;
     };
 
-    memcached.service = {
-      image = "memcached:1.6-alpine";
-      inherit networks;
-    };
+    services = {
 
-    postgres.service = {
-      image = "postgres:12-alpine";
-      volumes = [ "pgdata:/var/lib/postgresql/data" ];
-      environment.POSTGRES_PASSWORD = "productopener";
-      environment.POSTGRES_USER = "productopener";
-      environment.POSTGRES_DB = "minion";
-      inherit networks;
-    };
+      mongodb.service = {
+        image = "mongo:4.4";
+        volumes = [ "dbdata:/var/lib/mongodb" ];
+        inherit networks;
+      };
 
-    backend = {
-      nixos = {
-        useSystemd = true;
-        configuration = {
-          boot.tmpOnTmpfs = true;
-          services.httpd = {
-            enable = true;
-            # hostName = "productopener";
-            adminAddr = "productopener@example.org";
+      memcached.service = {
+        image = "memcached:1.6-alpine";
+        inherit networks;
+      };
+
+      postgres.service = {
+        image = "postgres:12-alpine";
+        volumes = [ "pgdata:/var/lib/postgresql/data" ];
+        environment = {
+          POSTGRES_PASSWORD = "productopener";
+          POSTGRES_USER = "productopener";
+          POSTGRES_DB = "minion";
+        };
+        inherit networks;
+      };
+
+      backend = {
+        nixos = {
+          useSystemd = true;
+          configuration = {
+            boot.tmpOnTmpfs = true;
+            services.httpd = {
+              enable = true;
+              # hostName = "productopener";
+              adminAddr = "productopener@example.org";
+              enablePerl = true;
+            };
+            # Copied over from the examples
+            # Not sure as to the reason why these are necessary
+            services.nscd.enable = false;
+            system.nssModules = lib.mkForce [ ];
           };
-          # Copied over from the examples
-          # Not sure as to the reason why these are necessary
-          services.nscd.enable = false;
-          system.nssModules = lib.mkForce [ ];
+        };
+        service = {
+          depends_on = [ "mongodb" "memcached" "postgres" ];
+          tmpfs = [ "/mnt/podata/mnt" ];
+          volumes = [
+            # "${toString ./.}:/opt/product-opener"
+            "podata:/mnt/podata"
+            # "${toString ./.}/backend-dev/conf/log.conf:/mnt/podata/log.conf"
+            # "${toString ./.}/backend-dev/conf/minion_log.conf:/mnt/podata/minion_log.conf"
+            # "${toString ./.}/backend-dev/conf/apache.conf:/etc/apache2/sites-enabled/product-opener.conf"
+          ];
+          # command = [
+          #   "${pkgs.bashinteractive}/bin/sh"
+          #   "${toString ./.}/backend-dev/conf/po-foreground.sh"
+          # ];
+          inherit networks;
         };
       };
-      service = {
-        depends_on = [ "mongodb" "memcached" "postgres" ];
+
+      frontend.service = {
+        image = "nginx:stable-alpine";
+        depends_on = [ "backend" ];
         volumes = [
-          "podata:/mnt/podata"
-          "product_images:/opt/product-opener/html/images/products"
-          "html_data:/opt/product-opener/html/data"
-          "${toString ./.}/backend-dev/conf/Config.pm:/opt/product-opener/lib/ProductOpener/Config.pm"
-          "${toString ./.}/backend-dev/conf/Config2.pm:/opt/product-opener/lib/ProductOpener/Config2.pm"
-          "${toString ./.}/backend-dev/conf/log.conf:/mnt/podata/log.conf"
-          "${toString ./.}/backend-dev/conf/minion_log.conf:/mnt/podata/minion_log.conf"
-          "${toString ./.}/backend-dev/conf/apache.conf:/etc/apache2/sites-enabled/product-opener.conf"
-          "${toString ./.}/backend-dev/conf/po-foreground.sh:/usr/local/bin/po-foreground.sh"
-        ];
-        command = [
-          "${pkgs.bashInteractive}/bin/sh"
-          "/usr/local/bin/po-foreground.sh"
+          "${toString ./.}/html:/opt/product-opener/html"
+          "${
+            toString ./.
+          }/frontend-git/conf/nginx.conf:/etc/nginx/conf.d/default.conf"
         ];
         inherit networks;
       };
-    };
-
-    # What is the best way to get the build Output from the npmlock2nix into this container?
-    frontend.service = {
-      image = "nginx:stable-alpine";
-      depends_on = [ "backend" ];
-      volumes = [
-        "product_images:/opt/product-opener/html/images/products"
-        "html_data:/opt/product-opener/html/data"
-        "${toString ./.}/frontend-git/conf/nginx.conf:/etc/nginx/conf.d/default.conf"
-      ];
     };
   };
 }
